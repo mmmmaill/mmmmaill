d19e20c7dc1e5c2c84340c0c4538911e851cbac9
mmmmaill <73694268+mmmmaill@users.noreply.github.com>
2020-10-30 17:31:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,23 @@
         <title>Hello world</title>
     </head>
     <body>
-        <h1>My First Heading</h1>
-        <p>My First Paragraph</p>
+        <h1>Apissara janpo</h1>
+        <p>My nickname is mail</p>
         <img src="cat.jpg">
+        <img src="cat.jpg">
+        
         <br>
-        <a href="http://www.it.kmitl.ac.th">This is a link</a>
+        <a href="https://www.pinterest.com/mariathorizon/%E0%B9%81%E0%B8%A1%E0%B8%A7/">This is a link</a>
 
         <h2>The unordered list</h2>
         <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
+            <li>มีเด็กคนหนึ่งแม่สั่งให้ไปซื้อขนมปังตอนเช้าทุกๆวัน มีอยู่วันหนึ่งเด็กคนนั้นไปซื้อขนมปังเช่นทุกวัน 
+เด็ก : ทำไมวันนี้ขนมปังเบาจังครับ 
+คนขาย : ลุงใส่น้อยๆหนูจะได้ไม่หนักไง 
+เด็กจึงให้เงินคนขายไป 3 บาท 
+คนขาย : ทำไมหนูให้เงินลุงแค่ 3 บาท ล่ะ ปกติต้องให้ 5 บาทไม่ใช่เหรอ 
+เด็ก : ก็ลุงจะได้นับเร็วๆไงครับ </li>
+           
         </ul>
 
     </body>
